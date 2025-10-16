840949727895a18f57c7514f7d3f7375e3f297c7
Time_Ripper <vadim090207@rambler.ru>
2025-10-16 21:56:21 +0300

    Add files via upload

---
+++ FileName.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int year;
+
+    printf("Введите год: ");
+
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("%d является високосным, следовательно в нём 366 дней", year);
+    }
+    else {
+        printf("%d является невисокосным, следовательно в нём 365 дней", year);
+    }
+
+    return 0;
+}
